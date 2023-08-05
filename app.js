c9df0e0e3bf4ccdd4a86f374fd60b893b8d86fc9
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-05 12:48:34 -0500

    feat: :sparkles: Creamos el endpoint de 'cliente'
    este enpoint nos trae la información de todos los clientes.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import appCliente from './routes/clientes.js';
 dotenv.config();
 let app = express();
 
 
-const config = JSON.parse(process.env.MY_SERVER);
+app.use(express.json());
+app.use('/clientes', appCliente);
 
+
+const config = JSON.parse(process.env.MY_SERVER);
 app.listen(config, () => {
     console.log(`http://${config.hostname}:${config.port}`);
 })
