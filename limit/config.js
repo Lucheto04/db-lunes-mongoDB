2e44faf5a4050258e4a1eb7f78d0325a94c85810
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-10 08:43:20 -0500

    refactor: :ambulance: Cambiamos el tamaño del json y el tiempo permitido para hacer las querys

--- limit/config.js
+++ limit/config.js
@@ -1,13 +1,13 @@
 import rateLimit from 'express-rate-limit';
 export let queryCliente = () => {
     return rateLimit({
-        windowMs: 15 * 60 * 1000,
-        max: 100,
+        windowMs: 30 * 1000,
+        max: 10,
         standardHeaders: true,
         legacyHeaders: false,
         // ! Toca cambiar el tamaño al json de la tabla cliente
         skip: (req, res) => {
-            if(req.headers["content-length"]>=90) {
+            if(req.headers["content-length"]>=150) {
                 res.status(413).send({
                     status: 413,
                     message: "El objeto que esta mandando supera el limite de tamaño, cambielo"
